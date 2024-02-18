44c45f521575be93fb9c8b4e6d7563533bcc5334
TZDSGeek <tysonzullo1010@gmail.com>
2024-02-17 22:57:26 -0700

    Almost ready for production

--- java.js
+++ java.js
@@ -1,1 +1,17 @@
-console.log(data)
+// Create a constant variable for the Json Data
+
+const jsonData = data;
+
+// Create Function for Charts Sample
+
+// function charts(sample) {
+
+let sampleData = jsonData[0]
+
+let employeeCount = sampleData.employees
+
+let profit = sampleData.profit_billions
+
+console.log(employeeCount,profit)
+
+// }
